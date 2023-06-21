55e3a99ecc9800651d52dec2e760f6ce860d4e25
Fredrik Wikstrom <fredrik@a500.org>
2023-06-21 12:45:54 +0300

    Fixed mkrelease.sh script.

--- mkrelease.sh
+++ mkrelease.sh
@@ -7,7 +7,7 @@ HOST="${1:-m68k-amigaos}"
 FORMAT="${2:-lha}"
 
 if [ "$HOST" = "m68k-amigaos" ]; then
-  make -f makefile.os3 all
+  make all
 else
   CPU=`echo "${HOST}" | cut -d'-' -f1`
   make -f makefile.aros CPU=${CPU} all
@@ -20,8 +20,8 @@ rm -rf ${DESTDIR}
 mkdir -p ${DESTDIR}/ssh2fs/L
 
 if [ "$HOST" = "m68k-amigaos" ]; then
-  cp -p Install-OS3 ${DESTDIR}/ssh2fs/Install
-#  cp -p README-OS3 ${DESTDIR}/ssh2fs/README
+  cp -p Install ${DESTDIR}/ssh2fs
+#  cp -p README ${DESTDIR}/ssh2fs
   cp -p bin/ssh2-handler.000 ${DESTDIR}/ssh2fs/L
   cp -p bin/ssh2-handler.020 ${DESTDIR}/ssh2fs/L
 else
@@ -33,12 +33,12 @@ cp -p LICENSE ${DESTDIR}/ssh2fs
 cp -p ${LIBSSH2}/COPYING ${DESTDIR}/ssh2fs/COPYING-libssh2
 #cp -p releasenotes ${DESTDIR}/ssh2fs
 
-cp -p icons-os3/def_drawer.info ${DESTDIR}/ssh2fs.info
-cp -p icons-os3/def_install.info ${DESTDIR}/ssh2fs/Install.info
-#cp -p icons-os3/def_doc.info ${DESTDIR}/ssh2fs/README.info
-cp -p icons-os3/def_doc.info ${DESTDIR}/ssh2fs/LICENSE.info
-cp -p icons-os3/def_doc.info ${DESTDIR}/ssh2fs/COPYING-libssh2.info
-#cp -p icons-os3/def_doc.info ${DESTDIR}/ssh2fs/releasenotes.info
+cp -p icons/def_drawer.info ${DESTDIR}/ssh2fs.info
+cp -p icons/def_install.info ${DESTDIR}/ssh2fs/Install.info
+#cp -p icons/def_doc.info ${DESTDIR}/ssh2fs/README.info
+cp -p icons/def_doc.info ${DESTDIR}/ssh2fs/LICENSE.info
+cp -p icons/def_doc.info ${DESTDIR}/ssh2fs/COPYING-libssh2.info
+#cp -p icons/def_doc.info ${DESTDIR}/ssh2fs/releasenotes.info
 
 case "${FORMAT}" in
   "7z")
